d2488aa3ab3e8d89717d2fae4864f60365ec671a
Бородкіна Наталія Григорівна <n.borodkina@bank.lan>
2019-06-12 16:46:01 +0300

    Add project files.

---
+++ Fraction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less6
+{
+    class Fraction
+    {
+
+        // поля
+        private int numer;
+        private int denomi;
+
+        //свойства
+        public int Numer { get => numer; set => numer = value; }
+        public int Denomi { get => denomi; set => denomi = (value < 0 ? 1 : value); }
+
+        //Конструктор
+        public Fraction(int n)
+        {
+            Numer = n;
+            Denomi = 1;
+        }
+
+        public Fraction(int n, int d):this(n)
+        {
+            Denomi = d;
+        }
+
+        //методы
+        public void ShowFractionInfo()
+        {
+            Console.WriteLine($"{Numer}/{Denomi}=" + (Numer / (double)Denomi));
+        }
+
+        public void ShowFractionReverse()
+        {
+            Console.WriteLine($"{Denomi}/{Numer}=" + ((double)Denomi / Numer));
+        }
+
+        public Fraction GetFractionReverse()
+        {
+            return new Fraction(Denomi, Numer);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Less6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(3);
+            fraction1.ShowFractionInfo();
+            fraction1.ShowFractionReverse();
+
+            Fraction fraction2 = fraction1.GetFractionReverse();
+            fraction2.ShowFractionInfo();
+
+        }
+    }
+}
